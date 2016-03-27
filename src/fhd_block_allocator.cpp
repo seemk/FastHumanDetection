4250bbb8b13ead91b2c25753f553e2b5bb93eb99
Siim Kallas <siimkallas@gmail.com>
2016-03-28 00:24:14 +0300

    Formatting fix.

--- src/fhd_block_allocator.cpp
+++ src/fhd_block_allocator.cpp
@@ -20,8 +20,10 @@ struct fhd_block_allocator {
   fhd_alloc_slot* full_slots;
 };
 
-fhd_block_allocator* fhd_block_allocator_create(int block_size, int num_blocks) {
-  fhd_block_allocator* block_alloc = (fhd_block_allocator*)calloc(1, sizeof(fhd_block_allocator));
+fhd_block_allocator* fhd_block_allocator_create(int block_size,
+                                                int num_blocks) {
+  fhd_block_allocator* block_alloc =
+      (fhd_block_allocator*)calloc(1, sizeof(fhd_block_allocator));
 
   block_alloc->block_size = block_size;
   block_alloc->num_bytes = block_size * num_blocks;
@@ -30,7 +32,8 @@ fhd_block_allocator* fhd_block_allocator_create(int block_size, int num_blocks) 
   block_alloc->free_indices_len = num_blocks;
   block_alloc->free_indices = (int*)calloc(num_blocks, sizeof(int));
   block_alloc->full_slots_len = 0;
-  block_alloc->full_slots = (fhd_alloc_slot*)calloc(num_blocks, sizeof(fhd_alloc_slot));
+  block_alloc->full_slots =
+      (fhd_alloc_slot*)calloc(num_blocks, sizeof(fhd_alloc_slot));
 
   for (int i = 0; i < num_blocks; i++) {
     block_alloc->free_indices[i] = i;
@@ -49,12 +52,13 @@ void fhd_block_allocator_destroy(fhd_block_allocator* block_alloc) {
 void* fhd_block_allocator_acquire(fhd_block_allocator* block_alloc) {
   if (block_alloc->free_indices_len == 0) return NULL;
 
-  const int free_idx = block_alloc->free_indices[block_alloc->free_indices_len - 1];
+  const int free_idx =
+      block_alloc->free_indices[block_alloc->free_indices_len - 1];
   const int offset = free_idx * block_alloc->block_size;
 
   uint8_t* block = block_alloc->memory + offset;
 
-  const fhd_alloc_slot slot = { block, free_idx };
+  const fhd_alloc_slot slot = {block, free_idx};
   block_alloc->full_slots[block_alloc->full_slots_len++] = slot;
 
   block_alloc->free_indices_len--;
@@ -63,12 +67,11 @@ void* fhd_block_allocator_acquire(fhd_block_allocator* block_alloc) {
 }
 
 void fhd_block_allocator_release(fhd_block_allocator* block_alloc, void* ptr) {
-
   for (int i = 0; i < block_alloc->full_slots_len; i++) {
     fhd_alloc_slot slot = block_alloc->full_slots[i];
     if (ptr == slot.ptr) {
       block_alloc->free_indices[block_alloc->free_indices_len] = slot.idx;
-      
+
       for (int j = i; j < block_alloc->full_slots_len - 1; j++) {
         block_alloc->full_slots[j] = block_alloc->full_slots[j + 1];
       }
@@ -83,7 +86,8 @@ void fhd_block_allocator_release(fhd_block_allocator* block_alloc, void* ptr) {
 
 void fhd_block_allocator_clear(fhd_block_allocator* block_alloc) {
   for (int i = 0; i < block_alloc->full_slots_len; i++) {
-    block_alloc->free_indices[block_alloc->free_indices_len++] = block_alloc->full_slots[i].idx;
+    block_alloc->free_indices[block_alloc->free_indices_len++] =
+        block_alloc->full_slots[i].idx;
   }
 
   block_alloc->full_slots_len = 0;
